9bdd859fa92b1473b6280224c2f6dafdaf529eb9
EC2 Default User <ec2-user@ip-172-31-34-38.us-east-2.compute.internal>
2019-03-07 23:20:47 +0000

    <complete 85 coverage>

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -1,3 +1,4 @@
+
 # TL;DR: YOU SHOULD DELETE THIS FILE
 #
 # This file is used by web_steps.rb, which you should also delete
@@ -14,7 +15,14 @@ module NavigationHelpers
     case page_name
 
     when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+     when /^the edit page for "([^"]+)"$/
+      edit_movie_path(Movie.find_by_title($1).id)
+      
+    when /^the details page for "([^"]+)"$/
+      movie_path(Movie.find_by_title($1).id)
 
+    when /^the Similar Movies page for "([^"]+)"$/
+      movie_similar_path(Movie.find_by_title($1).id)
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
